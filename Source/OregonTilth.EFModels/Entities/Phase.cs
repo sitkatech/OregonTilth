56b1df35df300749fdd3aebaf7144d3b48175774
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-03-15 08:10:25 -0700

    CCA-34 Users can enter Transplant Labor Activity by Crop * route for phases lookup table

---
+++ Source/OregonTilth.EFModels/Entities/Phase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OregonTilth.Models.DataTransferObjects;
+
+namespace OregonTilth.EFModels.Entities
+{
+    public partial class Phase
+    {
+        public static List<PhaseDto> List(OregonTilthDbContext dbContext)
+        {
+            return dbContext.Phases.Select(x => PhaseExtensionMethods.AsDto(x)).ToList();
+        }
+    }
+}
--- Source/OregonTilth.Web/src/app/services/lookup-tables/lookup-tables.service.ts
+++ Source/OregonTilth.Web/src/app/services/lookup-tables/lookup-tables.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from 'src/app/shared/services';
 import { Observable } from 'rxjs';
-import { WorkbookDto } from 'src/app/shared/models/generated/workbook-dto';
 import { FieldLaborActivityCategoryDto } from 'src/app/shared/models/generated/field-labor-activity-category-dto';
 import { LaborTypeDto } from 'src/app/shared/models/generated/labor-type-dto';
 import { FieldUnitTypeDto } from 'src/app/shared/models/generated/field-unit-type-dto';
+import { PhaseDto } from 'src/app/shared/models/generated/phase-dto';
 
 @Injectable({
     providedIn: 'root'
@@ -19,6 +19,11 @@ export class LookupTablesService {
 
     getLaborTypes(): Observable<LaborTypeDto[]> {
         let route = `/lookupTable/labor-types`;
+        return this.apiService.getFromApi(route);
+    }
+
+    getPhases(): Observable<PhaseDto[]> {
+        let route = `/lookupTable/phases`;
         return this.apiService.getFromApi(route);
     }
 
