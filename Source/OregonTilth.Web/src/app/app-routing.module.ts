ac181e7da0dd04978beec78cd70e976da5be4cc5
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-03-15 14:58:28 -0700

    CCA-35 User can enter Transplant Production Inputs * adding to side-nav and registering page route

--- Source/OregonTilth.Web/src/app/app-routing.module.ts
+++ Source/OregonTilth.Web/src/app/app-routing.module.ts
@@ -29,6 +29,7 @@ import { FieldLaborByCropComponent } from './pages/workbooks/forms/field-labor-b
 import { TransplantProductionLaborActivitiesComponent } from './pages/workbooks/forms/transplant-production-labor-activities/transplant-production-labor-activities.component';
 import { FieldInputCostsComponent } from './pages/workbooks/forms/field-input-costs/field-input-costs.component';
 import { TransplantProductionLaborByCropComponent } from './pages/workbooks/forms/transplant-production-labor-by-crop/transplant-production-labor-by-crop.component';
+import { TransplantProductionInputsComponent } from './pages/workbooks/forms/transplant-production-inputs/transplant-production-inputs.component';
 
 const routes: Routes = [
   { path: "labels-and-definitions/:id", component: FieldDefinitionEditComponent, canActivate: [UnauthenticatedAccessGuard, ManagerOnlyGuard, AcknowledgedDisclaimerGuard] },
@@ -49,6 +50,7 @@ const routes: Routes = [
   { path: "workbooks/:id/edit", component: EditWorkbookComponent},
   { path: "workbooks/:id/forms/field-labor-activities", component: FieldLaborActivitiesComponent},
   { path: "workbooks/:id/forms/transplant-production-labor-activities", component: TransplantProductionLaborActivitiesComponent},
+  { path: "workbooks/:id/forms/transplant-production-inputs", component: TransplantProductionInputsComponent},
   { path: "workbooks/:id/forms/machinery", component: MachineryComponent},
   { path: "workbooks/:id/forms/field-labor-by-crop", component: FieldLaborByCropComponent},
   { path: "workbooks/:id/forms/transplant-production-labor-by-crop", component: TransplantProductionLaborByCropComponent},
