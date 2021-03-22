c011766035f6cae78165fdd967780e892b59b725
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-03-22 15:39:35 -0700

    CCA-19 Users can record Field Standard Time Studies * removing a few unneeded th ings from release script

--- Database/ReleaseScript/0021 - adding StandardTimes  and FieldStandardTimes table.sql
+++ Database/ReleaseScript/0021 - adding StandardTimes  and FieldStandardTimes table.sql
@@ -44,29 +44,15 @@ CREATE TABLE dbo.FieldStandardTime(
 )
 
 
-CREATE TABLE dbo.TimeStudyType(
-	TimeStudyTypeID int NOT NULL CONSTRAINT PK_TimeStudyType_TimeStudyTypeID PRIMARY KEY,
-	TimeStudyTypeName varchar(100) NOT NULL CONSTRAINT AK_TimeStudyType_TimeStudyTypeName UNIQUE,
-	TimeStudyTypeDisplayName varchar(100) NOT NULL CONSTRAINT AK_TimeStudyType_TimeStudyTypeDisplayName UNIQUE
-)
-
-Insert into dbo.TimeStudyType (TimeStudyTypeID, TimeStudyTypeName, TimeStudyTypeDisplayName)
-values
-(1, 'FieldStandardTime', 'Field Standard Time'),
-(2, 'HarvestPostHarvestStandardTime', 'Harvest Post-Harvest Standard Time'),
-(3, 'TPStandardTime', 'TP Standard Time')
-
 CREATE TABLE dbo.TimeStudy(
     TimeStudyID int IDENTITY(1,1) NOT NULL CONSTRAINT PK_TimeStudy_TimeStudyID PRIMARY KEY,
     WorkbookID int NOT NULL CONSTRAINT FK_TimeStudy_Workbook_WorkbookID FOREIGN KEY REFERENCES dbo.Workbook (WorkbookID),
-    --TimeStudyTypeID int NOT NULL CONSTRAINT FK_TimeStudy_TimeStudyType_TimeStudyTypeID FOREIGN KEY REFERENCES dbo.TimeStudyType (TimeStudyTypeID),
+    
     FieldStandardTimeID int null CONSTRAINT FK_TimeStudy_FieldStandardTime_FieldStandardTimeID FOREIGN KEY REFERENCES dbo.FieldStandardTime (FieldStandardTimeID),
     Duration int not null,
     Units decimal not null,
     Notes varchar(8000) null,
-    --CONSTRAINT CK_TimeStudyType_Requires_FK_Checks CHECK (
-    --    (TimeStudyTypeID = 1 and FieldStandardTimeID is not null)
-    --)
+
 )
 
 
