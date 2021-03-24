11d50d7e2b5cbba411eaf35ccd35995361839d91
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-03-24 14:58:09 -0700

    CCA-41 Dashboard - Crop/Crop Unit * stubbing out bits of the calculation * ABSOLUTELY WILL NOT COMPILE IN THIS STATE

--- Source/OregonTilth.EFModels/Entities/CropYieldInformationExtensionMethods.cs
+++ Source/OregonTilth.EFModels/Entities/CropYieldInformationExtensionMethods.cs
@@ -18,5 +18,65 @@ namespace OregonTilth.EFModels.Entities
                 WorkbookID = cropYieldInformation.WorkbookID
             };
         }
+
+        public static decimal VariableCostPerMarketableUnit(this CropYieldInformation cropYieldInformation)
+        {
+
+            // =[@[TOTAL DIRECT VARIABLE COSTS]]/[@[Total Crop Units Sold]]
+
+
+            return cropYieldInformation.TotalDirectVariableCosts() / cropYieldInformation.MarketableYieldPerStandardUnitOfSpace;
+        }
+
+        public static decimal TotalDirectVariableCosts(this CropYieldInformation cropYieldInformation)
+        {
+            // =[@[TOTAL LABOR COSTS]]+[@[TOTAL NON-LABOR INPUT COSTS]]+[@[TOTAL MACHINERY COSTS]]
+
+            return cropYieldInformation.TotalLaborCosts() 
+                   + cropYieldInformation.TotalNonLaborInputCosts() 
+                   + cropYieldInformation.TotalMachineryCosts();
+
+        }
+
+        public static decimal TotalLaborCosts(this CropYieldInformation cropYieldInformation)
+        {
+            //=[@[TOTAL LABOR HOURS]]*INDEX(Table1[Average Hourly Wage],1)
+
+            return cropYieldInformation.TotalLaborHours() * (decimal) cropYieldInformation.Workbook.AverageHourlyWage;
+        }
+
+        public static decimal TotalLaborHours(this CropYieldInformation cropYieldInformation)
+        {
+            //=([@[FIELD LABOR HRS PER STD BED ]]
+            //+[@[HARVEST CREW LABOR HOURS PER STANDARD BED]]
+            //+[@[HARVEST OPERATOR LABOR HOURS PER STANDARD BED]]
+            //+[@[TRANSPLANT PRODUCTION LABOR HOURS PER STANDARD BED]]
+            //+[@[POST HARVEST LABOR HOURS PER STANDARD BED]])
+            //*[@[STANDARD Units of Space]]
+            
+
+            return (cropYieldInformation.FieldLaborHoursPerStandardBed() 
+                   + cropYieldInformation.HarvestCrewLaborHoursPerStandardBed() 
+                   + cropYieldInformation.HarvestOperatorLaborHoursPerStandardBed()
+                       + TransplantProductionLaborHoursPerStandardBed()
+                       +  PostHarvestLaborHoursPerStandardBed())
+                * cropYieldInformation.Workbook.StandardUnitsOfSpace()
+
+            
+            
+        }
+
+        public static decimal FieldLaborHoursPerStandardBed(this CropYieldInformation cropYieldInformation)
+        {
+            //=SUMIF(Table19[Crop],[@Crop],Table19[LABOR ACTIVITY MINUTES PER STANDARD BED])/60
+            
+            // sum the "FieldLaborByCrop"
+
+
+
+            return 0;
+        }
+
+
     }
 }
