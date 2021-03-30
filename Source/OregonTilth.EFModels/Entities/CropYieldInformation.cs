1bc7fc8ba07bf1f44b047cdb802d49287f3b5b3e
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-03-30 10:38:05 -0700

    Pairing w/ Tanya: * A few text changes * Very key calculation change

--- Source/OregonTilth.EFModels/Entities/CropYieldInformation.cs
+++ Source/OregonTilth.EFModels/Entities/CropYieldInformation.cs
@@ -128,7 +128,7 @@ namespace OregonTilth.EFModels.Entities
                 .Include(x => x.Crop).ThenInclude(x => x.CropSpecificInfos).ThenInclude(x => x.TpOrDsType)
                 .Include(x => x.Crop).ThenInclude(x => x.CropSpecificInfos).ThenInclude(x => x.Workbook)
                 .Include(x => x.Crop).ThenInclude(x => x.FieldLaborByCrops).ThenInclude(x => x.FieldLaborActivity).ThenInclude(x => x.FieldStandardTimes).ThenInclude(x => x.FieldUnitType)
-                .Include(x => x.Crop).ThenInclude(x => x.FieldInputByCrops).ThenInclude(x => x.FieldInputCost)
+                .Include(x => x.Crop).ThenInclude(x => x.FieldInputByCrops).ThenInclude(x => x.FieldInputCost).ThenInclude(x => x.FieldUnitType)
                 .Include(x => x.Crop).ThenInclude(x => x.TransplantProductionInformations).ThenInclude(x => x.Phase)
                 .Include(x => x.Crop).ThenInclude(x => x.TransplantProductionInformations).ThenInclude(x => x.TransplantProductionTrayType).ThenInclude(x => x.TransplantProductionInputCosts)
                 .Include(x => x.Crop).ThenInclude(x => x.TransplantProductionLaborActivityByCrops).ThenInclude(x => x.TransplantProductionLaborActivity).ThenInclude(x => x.TransplantProductionStandardTimes)
