4ad1451d0bd4f7113ed2c4c88886ccad88296567
stewartgordonsitka <stewart.gordon@sitkatech.com>
2021-08-06 11:54:46 -0700

    CCA-65 Duplicate a workbook * progress on duplication with procedure

--- Database/Objects/Procs/dbo.procDuplicateWorkbook.sql
+++ Database/Objects/Procs/dbo.procDuplicateWorkbook.sql
@@ -47,32 +47,73 @@ begin
 			OUTPUT src.MachineryID, inserted.MachineryID into @tempMachinery;
 
 		-- CROPS
-		INSERT INTO dbo.Crop ([WorkbookID], [CropName])
-			SELECT @NewWorkbookID, [CropName]
-				FROM dbo.Crop
-				WHERE WorkbookID = @WorkbookIDToCopy;
+		Declare @tempCrops table(
+			oID int
+			,nID int
+		);
 
+		merge dbo.Crop 
+		USING (
+			select CropID, [WorkbookID], CropName from dbo.Crop where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], CropName)
+			VALUES (@NewWorkbookID, CropName)
+			OUTPUT src.CropID, inserted.CropID into @tempCrops;
+			
 		-- CROP UNITS
-		INSERT INTO dbo.CropUnit ([WorkbookID], [CropUnitName])
-			SELECT @NewWorkbookID, [CropUnitName]
-				FROM dbo.CropUnit
-				WHERE WorkbookID = @WorkbookIDToCopy;
+		Declare @tempCropUnits table(
+			oID int
+			,nID int
+		);
+
+		merge dbo.CropUnit 
+		USING (
+			select CropUnitID, [WorkbookID], CropUnitName from dbo.CropUnit where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], CropUnitName)
+			VALUES (@NewWorkbookID, CropUnitName)
+			OUTPUT src.CropUnitID, inserted.CropUnitID into @tempCropUnits;
+
+		
 
 		-- TP LABOR ACTIVITIES
-		INSERT INTO [dbo].[TransplantProductionLaborActivity] ([WorkbookID], [TransplantProductionLaborActivityName])
-			SELECT @NewWorkbookID, [TransplantProductionLaborActivityName]
-				FROM dbo.[TransplantProductionLaborActivity]
-				WHERE WorkbookID = @WorkbookIDToCopy;
+
+		Declare @tempTransplantProductionLaborActivity table(
+			oID int
+			,nID int
+		);
+		merge dbo.[TransplantProductionLaborActivity] 
+		USING (
+			select TransplantProductionLaborActivityID, [WorkbookID], [TransplantProductionLaborActivityName] from dbo.[TransplantProductionLaborActivity] where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], [TransplantProductionLaborActivityName])
+			VALUES (@NewWorkbookID, [TransplantProductionLaborActivityName])
+			OUTPUT src.TransplantProductionLaborActivityID, inserted.TransplantProductionLaborActivityID into @tempTransplantProductionLaborActivity;
+
 
 		-- TP TRAY TYPES
-		INSERT INTO [dbo].[TransplantProductionTrayType] ([WorkbookID], [TransplantProductionTrayTypeName])
-			SELECT @NewWorkbookID, [TransplantProductionTrayTypeName]
-				FROM dbo.[TransplantProductionTrayType]
-				WHERE WorkbookID = @WorkbookIDToCopy;
+		Declare @tempTransplantProductionTrayType table(
+			oID int
+			,nID int
+		);
+
+		merge dbo.[TransplantProductionTrayType] 
+		USING (
+			select TransplantProductionTrayTypeID, [WorkbookID], [TransplantProductionTrayTypeName] from dbo.[TransplantProductionTrayType] where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], [TransplantProductionTrayTypeName])
+			VALUES (@NewWorkbookID, [TransplantProductionTrayTypeName])
+			OUTPUT src.TransplantProductionTrayTypeID, inserted.TransplantProductionTrayTypeID into @tempTransplantProductionTrayType;
+
+
 
 		-- TP INPUTS
 
-		Declare @tempTpInput table(
+		Declare @tempTransplantProductionInput table(
 			oID int
 			,nID int
 		);
@@ -84,24 +125,75 @@ begin
 		WHEN NOT MATCHED THEN 
 			INSERT([WorkbookID], [TransplantProductionInputName])
 			VALUES (@NewWorkbookID, src.TransplantProductionInputName)
-			OUTPUT src.TransplantProductionInputID, inserted.TransplantProductionInputID into @tempTpInput;
+			OUTPUT src.TransplantProductionInputID, inserted.TransplantProductionInputID into @tempTransplantProductionInput;
 
 
 
 		-- TP INPUT COSTS
-		INSERT INTO [dbo].[TransplantProductionInputCost] ([WorkbookID], [TransplantProductionInputID], [TransplantProductionTrayTypeID], [CostPerTray], [Notes])
-		SELECT @NewWorkbookID, [TransplantProductionInputID], [TransplantProductionTrayTypeID], [CostPerTray], [Notes]
-			FROM dbo.[TransplantProductionInputCost]
-			WHERE WorkbookID = @WorkbookIDToCopy;
+		Declare @tempTransplantProductionInputCost table(
+			oID int
+			,nID int
+		);
+
+		merge dbo.[TransplantProductionInputCost] 
+		USING (
+			select TransplantProductionInputCostID, [TransplantProductionInputID], [TransplantProductionTrayTypeID], [CostPerTray], [Notes] from dbo.[TransplantProductionInputCost] where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], [TransplantProductionInputID], [TransplantProductionTrayTypeID], [CostPerTray], [Notes])
+			VALUES (@NewWorkbookID, 
+			(select nID from @tempTransplantProductionInput where oID = src.TransplantProductionInputID),
+			(select nID from @tempTransplantProductionTrayType where oID = src.TransplantProductionTrayTypeID),
+			src.CostPerTray,
+			src.Notes)
+			OUTPUT src.TransplantProductionInputCostID, inserted.TransplantProductionInputCostID into @tempTransplantProductionInputCost;
 
 
-		-- FIELD LABOR TIME STUDIES
+		
 
 
+		-- FIELD STANDARD TIME
+		/*
+		Declare @tempFieldStandardTime table(
+			oID int
+			,nID int
+		);
 
+		merge dbo.FieldStandardTime 
+		USING (
+			select [FieldStandardTimeID], [WorkbookID], [FieldLaborActivityID], [LaborTypeID], [MachineryID], [FieldUnitTypeID], [StandardTimePerUnit] from dbo.FieldStandardTime where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], [FieldLaborActivityID], [LaborTypeID], [MachineryID], [FieldUnitTypeID], [StandardTimePerUnit])
+			VALUES (@NewWorkbookID, 
+			(select nID from @tempFieldLaborActivity where oID = FieldLaborActivityID),
+			[LaborTypeID], 
+			(select nID from @tempMachinery where oID = MachineryID),
+			[FieldUnitTypeID], [StandardTimePerUnit])
+			OUTPUT src.FieldStandardTimeID, inserted.FieldStandardTimeID into @tempFieldStandardTime;
 
+		*/
 
 		-- HARVEST TIME STUDIES
+		/*
+		Declare @tempHarvestPostHarvestStandardTime table(
+			oID int
+			,nID int
+		);
+
+		merge dbo.HarvestPostHarvestStandardTime 
+		USING (
+			select [HarvestPostHarvestStandardTimeID], [WorkbookID], [CropID], [CropUnitID], [HarvestTypeID], [StandardTimePerUnit] from dbo.HarvestPostHarvestStandardTime where WorkbookID = @WorkbookIDToCopy
+		) as src on 1=0
+		WHEN NOT MATCHED THEN 
+			INSERT([WorkbookID], [CropID], [CropUnitID], [HarvestTypeID], [StandardTimePerUnit])
+			VALUES (@NewWorkbookID, 
+			(select nID from @tempFieldLaborActivity where oID = FieldLaborActivityID),
+			[LaborTypeID], 
+			(select nID from @tempMachinery where oID = MachineryID),
+			[FieldUnitTypeID], [StandardTimePerUnit])
+			OUTPUT src.HarvestPostHarvestStandardTimeID, inserted.HarvestPostHarvestStandardTimeID into @tempHarvestPostHarvestStandardTime;
+			*/
 
 		-- TP TIME STUDIES
 
@@ -126,4 +218,4 @@ begin
 end
 go
 
--- exec dbo.procDuplicateWorkbook @WorkbookIDToCopy = 2, @WorkbookCopyName = 'real'
+-- exec dbo.procDuplicateWorkbook @WorkbookIDToCopy = 2, @WorkbookCopyName = 'best'
