320aa8f95712babc5281b71b30555194fc8fa22b
stewartgordonsitka <stewart.gordon@sitkatech.com>
2024-10-07 09:16:48 -0700

    * remove app insights

--- OregonTilth.API/Startup.cs
+++ OregonTilth.API/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using IdentityServer4.AccessTokenValidation;
-using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.DependencyCollector;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,41 +12,22 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using OregonTilth.API.Services;
 using OregonTilth.API.Services.Hierarchy;
-using OregonTilth.API.Services.Telemetry;
 using OregonTilth.EFModels.Entities;
 using Serilog;
-using Serilog.Sinks.ApplicationInsights.Sinks.ApplicationInsights.TelemetryConverters;
 using System;
-using System.Linq;
 using System.Net.Http;
-using System.Threading;
 using ILogger = Serilog.ILogger;
 
 namespace OregonTilth.API
 {
     public class Startup
     {
-        private readonly TelemetryClient _telemetryClient;
         private readonly IWebHostEnvironment _environment;
-        private string _instrumentationKey;
         public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
             Configuration = configuration;
             _environment = environment;
 
-            _instrumentationKey = Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
-
-            if (!string.IsNullOrWhiteSpace(_instrumentationKey))
-            {
-                _telemetryClient = new TelemetryClient(TelemetryConfiguration.CreateDefault())
-                {
-                    InstrumentationKey = _instrumentationKey
-                };
-            }
-            else
-            {
-                _telemetryClient = new TelemetryClient(TelemetryConfiguration.CreateDefault());
-            }
         }
 
         public IConfiguration Configuration { get; }
@@ -57,7 +35,6 @@ namespace OregonTilth.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationInsightsTelemetry(_instrumentationKey);
             services.AddControllers().AddNewtonsoftJson(opt =>
                 {
                     if (!_environment.IsProduction())
@@ -109,11 +86,7 @@ namespace OregonTilth.API
             });
 
             services.AddSingleton(Configuration);
-            services.AddSingleton<ITelemetryInitializer, CloudRoleNameTelemetryInitializer>();
-            services.AddSingleton<ITelemetryInitializer, UserInfoTelemetryInitializer>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            var logger = GetSerilogLogger();
-            services.AddSingleton(logger);
 
             services.AddTransient(s => new KeystoneService(s.GetService<IHttpContextAccessor>(), keystoneHost));
 
@@ -153,44 +126,16 @@ namespace OregonTilth.API
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.Use(TelemetryHelper.PostBodyTelemetryMiddleware);
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
 
-            applicationLifetime.ApplicationStopping.Register(OnShutdown);
-
-            var modules = app.ApplicationServices.GetServices<ITelemetryModule>();
-            var dependencyModule = modules.OfType<DependencyTrackingTelemetryModule>().FirstOrDefault();
-
-            if (dependencyModule != null)
-            {
-                var domains = dependencyModule.ExcludeComponentCorrelationHttpHeadersOnDomains;
-                domains.Add("core.windows.net");
-                domains.Add("10.0.75.1");
-            }
+            
         }
-        private void OnShutdown()
-        {
-            _telemetryClient.Flush();
-            Thread.Sleep(1000);
-        }
+        
 
-        private ILogger GetSerilogLogger()
-        {
-            var outputTemplate = $"[{_environment.EnvironmentName}] {{Timestamp:yyyy-MM-dd HH:mm:ss zzz}} {{Level}} | {{RequestId}}-{{SourceContext}}: {{Message}}{{NewLine}}{{Exception}}";
-            var serilogLogger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
-                .WriteTo.Console(outputTemplate: outputTemplate);
-
-            if (!_environment.IsDevelopment())
-            {
-                serilogLogger.WriteTo.ApplicationInsights(_telemetryClient, new TraceTelemetryConverter());
-            }
-
-            return serilogLogger.CreateLogger();
-        }
+       
     }
 }
