eff2ae304d643638dd32f07844828e2744211b5c
stewartgordonsitka <stewart.gordon@sitkatech.com>
2024-10-07 11:39:47 -0700

    * add system info controller for root path

---
+++ OregonTilth.Models/DataTransferObjects/SystemInfoDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace OregonTilth.Models.DataTransferObjects;
+
+public class SystemInfoDto
+{
+    public string Environment { get; set; }
+    public string Version { get; set; }
+    public string CurrentTimeUTC { get; set; }
+    public DateTime CompilationDateTime { get; set; }
+    public string Application => Assembly.GetEntryAssembly().GetName().Name;
+
+    public SystemInfoDto()
+    {
+        var assemblyVersion = Assembly.GetEntryAssembly()!.GetName().Version;
+        Version = $"{assemblyVersion!.Major}.{assemblyVersion!.Minor}.{assemblyVersion!.Build}";
+
+        var localAssemblyPathString = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
+        var fileInfo = new FileInfo(localAssemblyPathString);
+        CompilationDateTime = fileInfo.LastWriteTime;
+    }
+}
